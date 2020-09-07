61fbf339886240acf2fd566ad18644ff4d42d98d
SatvikGulati2006 <70364052+SatvikGulati2006@users.noreply.github.com>
2020-09-07 12:20:01 +0530

    algorithm

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var fixedrect ,movingrect;
+function setup() {
+  createCanvas(800,400);
+  fixedrect =createSprite(400, 200, 30, 80);
+  fixedrect.shapeColor ="blue";
+  movingrect =createSprite(200, 200, 80, 30);
+  movingrect.shapeColor ="blue";
+}
+
+function draw() {
+  background(255,255,255);  
+
+  movingrect.x = World.mouseX;
+  movingrect.y = World.mouseY; 
+  
+  if(movingrect.x-fixedrect.x < fixedrect.width/2 +movingrect.width/2 
+    && fixedrect.x-movingrect.x <fixedrect.width/2+movingrect.width/2
+    &&movingrect.y-fixedrect.y < fixedrect.height/2 +movingrect.height/2 
+    && fixedrect.y-movingrect.y<fixedrect.height/2+movingrect.height/2)
+    { movingrect.shapeColor="red"; fixedrect.shapeColor ='red'; }
+     else {fixedrect.shapeColor ='green'; movingrect.shapeColor ='green'; }
+
+  drawSprites();
+}
